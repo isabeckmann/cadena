0c83a5c6e0d60fb08b72be1691437a8bf0397c03
Isadora Beckmann <beckmannisadora@gmail.com>
2023-04-23 15:37:03 -0300

    finalização do switch mode - tema dark

--- js/main.js
+++ js/main.js
@@ -3,4 +3,24 @@ const $checkbox = document.querySelector("#switch");
 
 $checkbox.addEventListener("change", function () {
   $html.classList.toggle("dark-mode");
+
+  if (this.checked) {
+    document.getElementById("logo-switch").src = "../images/logo.white.svg";
+    document.getElementById("logo-footer-sw").src = "../images/logo.white.svg";
+  } else {
+    document.getElementById("logo-switch").src = "../images/Union.svg";
+    document.getElementById("logo-footer-sw").src = "../images/Union.svg";
+  }
+
+  if (this.checked) {
+    document.getElementById("seta-sw").src = "../images/seta.dark.svg";
+  } else {
+    document.getElementById("seta-sw").src = "../images/Vector.svg";
+  }
+
+  if (this.checked) {
+    document.getElementById("cards-switch").src = "../images/cards.dark.png";
+  } else {
+    document.getElementById("cards-switch").src = "../images/cards.png";
+  }
 });
